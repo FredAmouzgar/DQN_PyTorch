cbf5937f82e5f0126245ed633481e4a0592bfa5a
Fred <realfred.amouzgar@gmail.com>
2020-08-19 21:08:11 +1000

    Replacing prints with tqdm progress bar

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ from src.agent import Agent
 from collections import deque
 import numpy as np
 import torch
+from tqdm import tqdm
 
 def train(agent, env, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
     """Deep Q-Learning.
@@ -19,7 +20,8 @@ def train(agent, env, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, 
     brain_name = env.brain_names[0]
     scores_window = deque(maxlen=100)  # last 100 scores
     eps = eps_start  # initialize epsilon
-    for i_episode in range(1, n_episodes + 1):
+    episode_loop = tqdm(range(1, n_episodes + 1), desc="Episode 0 | Avg Score: None", leave=False)
+    for i_episode in episode_loop:
         # reset the environment
         env_info = env.reset(train_mode=True)[brain_name]
         state = env_info.vector_observations[0]
@@ -36,12 +38,15 @@ def train(agent, env, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, 
         scores_window.append(score)  # save most recent score
         scores.append(score)  # save most recent score
         eps = max(eps_end, eps_decay * eps)  # decrease epsilon
-        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
-        if i_episode % 100 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        #print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        #if episode_loop.n % 100 == 0:
+        # print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        episode_loop.set_description_str('Episode {} | Avg Score: {:.2f}'.format(episode_loop.n, np.mean(scores_window)))
+
         if np.mean(scores_window) > prior_score:
-            print('\nEnvironment reached in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100,
-                                                                                         np.mean(scores_window)))
+            # print('\nEnvironment reached in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100,
+            #                                                                             np.mean(scores_window)))
+            episode_loop.set_description_str('Achieved in {:d} episode | Avg Score: {:.2f}'.format(episode_loop.n - 100,np.mean(scores_window)))
             torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
             prior_score = np.mean(scores_window)
     return scores
